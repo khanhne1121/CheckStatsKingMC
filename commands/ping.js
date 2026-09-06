cf964b57601166c1b71d9f96036031a931a4d9b3
khanhne1121 <khanhtrong1119@gmail.com>
2026-09-06 23:37:21 +0700

    Update footer text in ping command

--- commands/ping.js
+++ commands/ping.js
@@ -69,7 +69,7 @@ module.exports = {
       .setColor('#2b2d31')
       .setDescription(`\`\`\`text\n${lines}\n\`\`\``)
       .setTimestamp()
-      .setFooter({ text: 'KingMC.vn Stats Bot • Thiết kế bởi BinhLH' });
+      .setFooter({ text: 'KingMC.vn Stats Bot • By Kian Nguyen' });
 
     if (interaction.editReply) {
       await interaction.editReply({ embeds: [embed] });
