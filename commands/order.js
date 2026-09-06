0dac7cf8391896b1ba9026cf93c9e50fa2cffbb2
khanhne1121 <khanhtrong1119@gmail.com>
2026-09-07 00:50:14 +0700

    Update footer text in order.js

--- commands/order.js
+++ commands/order.js
@@ -38,7 +38,7 @@ module.exports = {
           .setDescription(`⚠️ Không có order (đơn hàng) nào cho **${itemDisplayName}**.`)
           .setColor('#ef4444')
           .setTimestamp()
-          .setFooter({ text: 'KingMC.vn Stats Bot • Thiết kế bởi BinhLH' });
+          .setFooter({ text: 'KingMC.vn Stats Bot • By Kian Nguyen' });
 
         return await interaction.editReply({ embeds: [emptyEmbed] });
       }
